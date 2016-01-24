46edefb4cb27b5bd6272392453db6daa5efa05a5
Joel Rahman <joelrahman@Joels-Laptop.local>
2016-01-25 08:56:01 +1100

    Scaffolded

---
+++ app/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ausEnvApp
+ * @description
+ * # ausEnvApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ausEnvApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngResource',
+    'ngRoute',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ausEnvApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ausEnvApp
+ */
+angular.module('ausEnvApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
