eb8eb4555e6a74ddce0acef99e9221ce6cf29fc3
Zhenyue Qin <u5505995@anu.edu.au>
2016-01-27 08:38:39 +1100

    Basic Maps
    Put the map into the framework with a WMS.
    Fixed the Warning: Task "jshint:all" failed. Use --force to continue.

--- app/scripts/controllers/map.js
+++ app/scripts/controllers/map.js
@@ -7,8 +7,57 @@
  * # MapCtrl
  * Controller of the ausEnvApp
  */
+
 angular.module('ausEnvApp')
   .controller('MapCtrl', function ($scope,selection) {
     $scope.selection = selection;
 
+    angular.extend($scope,{
+    	
+      	defaults:{
+        	crs: L.CRS.EPSG4326
+      	},
+      	
+      	mapCentre:{
+        	lat: -23.07,
+        	lng: 135.08,
+        	zoom: 4
+      	},
+      layers:{
+        baselayers:{
+          osm: {
+            name: 'OpenStreetMap',
+            url: 'http://129.206.228.72/cached/osm?',
+            type: 'wms',
+            layerParams:{
+              version: '1.1.1',
+              format: 'image/png',
+              layers:'osm_auto:all'
+            }
+          },
+        },
+        overlays:{
+          aWMS:{
+            // http://dapds00.nci.org.au/thredds/wms/ub8/global/nc/1d/actual/Stot/20150920_Stot_daily.nc?service=WMS&version=1.3.0&request=GetCapabilities
+            // http://dapds00.nci.org.au/thredds/wms/ub8/OzWALD/AWRA.run20160107.daily.D.2000.01.nc?service=WMS&version=1.3.0&request=GetCapabilities
+            name: 'Some Model Results',
+            type: 'wms',
+            visible: true,
+            url: 'http://dapds00.nci.org.au/thredds/wms/ub8/global/nc/1d/actual/Stot/20150920_Stot_daily.nc?',
+            doRefresh: true,
+            layerParams: {
+              layers: 'Stot_daily',
+              time:'2015-09-20',
+              format: 'image/png',
+              transparent: true,
+              logscale:false,
+              styles:'boxfill/rainbow',
+              numcolorbands:50,
+              colorscalerange:'0,1000',
+              belowmincolor:'extend',abovemaxcolor:'extend'
+            }
+          }
+        }
+      }
+    });
   });
