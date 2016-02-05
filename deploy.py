153b0a5df17fd9091c3083c74e691cf5b3ed8fab
Zac-HD <zac.hatfield.dodds@gmail.com>
2016-02-05 17:30:00 +1100

    Prototype deployment script

---
+++ deploy.py
@@ -0,0 +1,13 @@
+import ftplib
+import os
+
+ftp = ftplib.FTP('ftp.wenfo.org', os.environ['FTP_USER'], os.environ['FTP_PASSWORD'])
+ftp.cwd('/public_html/aus-env/')
+
+for root, _, files in os.walk('dist'):
+    for f in files:
+        file = os.path.join(root, f)
+        with open(file, "rb") as fobj:
+            ftp.storbinary("STOR " + file, fobj)
+
+ftp.quit()
