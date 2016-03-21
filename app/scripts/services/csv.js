1b29f66fc5b4ae91b046b29bf3db69d8b2027634
Joel Rahman <joel@flowmatters.com.au>
2016-03-22 06:48:48 +1100

    Refactored CSV parsing to its own service. Moved tests

---
+++ app/scripts/services/csv.js
@@ -0,0 +1,77 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name ausEnvApp.csv
+ * @description
+ * # csv
+ * Service in the ausEnvApp.
+ */
+angular.module('ausEnvApp')
+  .service('csv', function () {
+    var service = this;
+
+    service.parseKeyValueHeader = function(text) {
+      var lines = text.split?text.split('\n'):text;
+      var result = {};
+
+      lines.forEach(function(line){
+        var components = line.split(',');
+        var key = components.shift();
+        var value = components.join(',');
+        result[key] = value;
+      });
+
+      return result;
+    };
+
+    service.parseRegularCSV = function(text,idPrefix,asRecord){
+      var idPrefix = (idPrefix===undefined)? 'PlaceIndex' : idPrefix;
+      var data = {};
+      var lines = text.split('\n');
+      var header = lines.shift();
+      var columns = header.split(',').map(Function.prototype.call,String.prototype.trim);;
+      columns.shift();
+
+      var parseValue = function(val){
+        if(val==='-9999'){
+          return null;
+        }
+
+        var num = +val;
+        if(isNaN(num)){
+          return val.trim();
+        }
+        return num;
+      };
+
+      lines.forEach(function(line){
+        var cols = line.trim().split(',');
+        var polygonIdentifier = idPrefix + cols.shift();
+
+        if(asRecord) {
+          data[polygonIdentifier] = {};
+          cols.forEach(function(val,idx){
+            data[polygonIdentifier][columns[idx]] = parseValue(val);
+          });
+        } else {
+          data[polygonIdentifier] = cols.map(parseValue); //convert the numbers of type string into the actual numbers
+        }
+      });
+      data.columnNames = columns;
+      return data;
+    };
+
+    service.parseCSVWithHeader = function(text,idPrefix,asRecord){
+        var sections = text.split(/\n\-+\s*\n/);
+        var data = null;
+        if(sections.length>1){
+          data = service.parseKeyValueHeader(sections.shift());
+        } else {
+          data = {};
+        }
+
+        angular.extend(data,service.parseRegularCSV(sections[0],idPrefix,asRecord));
+        return data;
+    };
+  });
--- app/scripts/services/details.js
+++ app/scripts/services/details.js
@@ -11,75 +11,12 @@
 //selection.regionType
 //selection.selectedLayer
 angular.module('ausEnvApp')
-  .service('details', function ($q,$http,staticData) {
+  .service('details', function ($q,$http,$window,staticData,csv) {
     var service = this;
 
+    service.dap = $window.dap;
     service.MAX_CACHE_LENGTH=20;
     service.cache = [];
-
-    service.parseKeyValueHeader = function(text) {
-      var lines = text.split?text.split('\n'):text;
-      var result = {};
-
-      lines.forEach(function(line){
-        var components = line.split(',');
-        var key = components.shift();
-        var value = components.join(',');
-        result[key] = value;
-      });
-
-      return result;
-    };
-
-    service.parseRegularCSV = function(text,idPrefix,asRecord){
-      var idPrefix = (idPrefix===undefined)? 'PlaceIndex' : idPrefix;
-      var data = {};
-      var lines = text.split('\n');
-      var header = lines.shift();
-      var columns = header.split(',').map(Function.prototype.call,String.prototype.trim);;
-      columns.shift();
-
-      var parseValue = function(val){
-        if(val==='-9999'){
-          return null;
-        }
-
-        var num = +val;
-        if(isNaN(num)){
-          return val.trim();
-        }
-        return num;
-      };
-
-      lines.forEach(function(line){
-        var cols = line.trim().split(',');
-        var polygonIdentifier = idPrefix + cols.shift();
-
-        if(asRecord) {
-          data[polygonIdentifier] = {};
-          cols.forEach(function(val,idx){
-            data[polygonIdentifier][columns[idx]] = parseValue(val);
-          });
-        } else {
-          data[polygonIdentifier] = cols.map(parseValue); //convert the numbers of type string into the actual numbers
-        }
-      });
-      data.columnNames = columns;
-      return data;
-    };
-
-    service.parseCSVWithHeader = function(text,idPrefix,asRecord){
-        var sections = text.split(/\n\-+\s*\n/);
-        var data = null;
-        if(sections.length>1){
-          data = service.parseKeyValueHeader(sections.shift());
-        } else {
-          data = {};
-        }
-
-        angular.extend(data,service.parseRegularCSV(sections[0],idPrefix,asRecord));
-        return data;
-    };
 
     service.retrieveCSV = function(url){
       var existing = service.cache.filter(function(entry){
@@ -94,7 +31,7 @@ angular.module('ausEnvApp')
       var httpPromise = $http.get(url);
 
       httpPromise.then(function(resp){
-        var data = service.parseCSVWithHeader(resp.data);
+        var data = csv.parseCSVWithHeader(resp.data);
         result.resolve(data);
       });
 
@@ -132,7 +69,7 @@ angular.module('ausEnvApp')
     };
 
     service.landCoverCodes = staticData.deferredGet(service,'static/config/DLCD_codes.csv','_landcoverText',function(text){
-      return service.parseRegularCSV(text,'',true);
+      return csv.parseRegularCSV(text,'',true);
     });
 
     //<editor-fold desc="pete linegraph">
