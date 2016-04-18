66fc23ec998c3f0e0f643decfd356c525d54709a
Peter Manger <peter.k.manger@gmail.com>
2016-04-18 18:04:18 +1000

    Added some polyfil to for IE. Fix for array.find ... but, after this was put in we get a problem with addControl in leaflet "addTo" is not a property or method.  However I cannot see any issue and the legend appears properly now and all the other controls are ok.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -26,7 +26,31 @@ Math.log10 = Math.log10 || function(x) {
     return Math.log(x) / Math.LN10;
   };
 
+// Array.find polyfil for IE
+if (!Array.prototype.find) {
+  Array.prototype.find = function(predicate) {
+    if (this === null) {
+      throw new TypeError('Array.prototype.find called on null or undefined');
+    }
+    if (typeof predicate !== 'function') {
+      throw new TypeError('predicate must be a function');
+    }
+    var list = Object(this);
+    var length = list.length >>> 0;
+    var thisArg = arguments[1];
+    var value;
 
+    for (var i = 0; i < length; i++) {
+      value = list[i];
+      if (predicate.call(thisArg, value, i, list)) {
+        return value;
+      }
+    }
+    return undefined;
+  };
+}
+
+// startsWith polyfil
 if (!String.prototype.startsWith) {
   String.prototype.startsWith = function(searchString, position){
     position = position || 0;
