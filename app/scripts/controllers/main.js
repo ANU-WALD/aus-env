28029d8e0352a3a9ea173b8da1ec977fb74e3f14
Joel Rahman <joel.rahman.home@gmail.com>
2016-11-04 10:17:07 +1100

    Placeholder splash screen

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,12 +8,34 @@
  * Controller of the ausEnvApp
  */
 angular.module('ausEnvApp')
-  .controller('MainCtrl', function ($scope,selection) {
+  .controller('MainCtrl', function ($scope,$uibModal,selection) {
     $scope.selection = selection;
+    $scope.options = {
+      doNotShow:false
+    };
 
     $scope.moveYear = function(dir){
       $scope.selection.moveYear(dir);
     };
 
     $scope.runningLocally = (window.location.hostname==='localhost');
+
+    var options = {
+      animation:true,
+      templateUrl: 'views/about.html',
+      scope: $scope
+    };
+
+    if(!localStorage.preventAbout){
+      $uibModal.open(options);
+    }
+
+    $scope.doNotShowClicked = function(){
+      console.log($scope.options);
+      if($scope.options.doNotShow){
+        localStorage.setItem('preventAbout',true);
+      } else {
+        localStorage.removeItem('preventAbout');
+      }
+    };
   });
