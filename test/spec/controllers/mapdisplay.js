c3de101b051749a37a8e703790bfef34b5a688ef
Joel Rahman <joel.rahman.home@gmail.com>
2016-12-05 14:08:46 +1100

    remove scaffolded test

--- test/spec/controllers/mapdisplay.js
+++ test/spec/controllers/mapdisplay.js
@@ -16,8 +16,4 @@ describe('Controller: MapdisplayCtrl', function () {
       // place here mocked dependencies
     });
   }));
-
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(MapdisplayCtrl.awesomeThings.length).toBe(3);
-  });
 });
