113e4e2ef5032ea44b69c313641ecce5d7a02cfd
Joel Rahman <joel@flowmatters.com.au>
2019-09-30 15:24:10 +1000

    Switch pie chart to plotly

--- app/scripts/controllers/pie.js
+++ app/scripts/controllers/pie.js
@@ -8,16 +8,29 @@
  * Controller of the ausEnvApp
  */
 angular.module('ausEnvApp')
-  .controller('PieCtrl', function ($scope,details,downloads,selection) {
+  .controller('PieCtrl', function ($scope,$element,$timeout,details,downloads,selection) {
     $scope.pie = details.chartMetaData();
     $scope.pie.sum=null;
     $scope.pieData = [];
     $scope.pieLabels = [];
 
     $scope.pieChartOptions = {
+      responsive: false,
       animateRotate: false,
       animationSteps: 1,
       tooltipTemplate: details.tooltipTextFunction($scope.pie)
+    };
+
+    $scope.clearChart = function(){
+      $scope.pie = details.chartMetaData();
+      $scope.pieData=[];
+      $scope.pieLabels = [];
+      $scope.pie.loading=false;
+
+      var target = $element[0];
+      target = $('.pie-chart',target)[0];
+
+      Plotly.purge(target);
     };
 
     $scope.createChart = function(){
@@ -26,38 +39,73 @@ angular.module('ausEnvApp')
 
       if(selection.selectedLayer&&selection.selectedLayer.disablePie){
         $scope.chartView('pie',false);
-        $scope.pie = details.chartMetaData();
-        $scope.pieData=[];
-        $scope.pieLabels = [];
-        $scope.pie.loading=false;
+        $scope.clearChart();
         return;
       }
 
       $scope.getPieData().then(function(data){
         var _ = window._;
+
+        if(!$scope.selection.graphVisible.pie){
+          $scope.clearChart();
+          return;
+        }
+
         var series = data[0];
         var labels = data[1];
         var colours = data[2];
         var metadata = data[3];
 
+        var target = $element[0];
+        target = $('.pie-chart',target)[0];
+
+
+        $scope.pieChartOptions.responsive = true;
         $scope.pieData = series;
         $scope.pieLabels = labels;
         $scope.pieColours = colours;
         if(!selection.selectedLayer.disableAnnual){
           $scope.pie.year = selection.year;
         }
+        $timeout(function(){
+          $(target).one('plotly_afterplot', function(){
+            details.populateLabels($scope.pie,metadata);
+            $scope.pie.loading=false;
+          });
+          Plotly.newPlot( target, [{
+            labels: labels,
+            values: series,
+            type: 'pie',
+            sort:false,
+            marker:{
+              colors:colours
+            },
+            text: series.map(function(s,i){return labels[i]+':<br> '+ details.formatValue(s) + $scope.pie.units;}),
+            hoverinfo: 'text',
+            textinfo: 'none',
+          }], {
+            height: 200,
+            width: 220,
+            margin: {
+              l:40,
+              r:10,
+              b:30,
+              t:10
+            },
+            showlegend:false
+          },{
+            modeBarButtonsToRemove: ['hoverCompareCartesian','hoverClosestCartesian','lasso2d','select2d'],
+            displaylogo: false
+          });
+          Plotly.relayout( target, {});
+        });
 
         details.populateLabels($scope.pie,metadata);
         $scope.pie.sum = $scope.pieData.reduce(function(x,y){return x+y;}).toFixed();
         $scope.pie.download = downloads.downloadableTable(_.zip(labels,series),['Land Cover','Value']);
         $scope.pie.download_fn = downloads.makeDownloadFilename($scope.locationLabel(),$scope.pie.title,selection.year+'_by_landcover_classification');
         $scope.pie.loading=false;
-      },function(){
-        $scope.pie = details.chartMetaData();
-        $scope.pieData=[];
-        $scope.pieLabels = [];
-        $scope.pie.loading=false;
-      });
+      },$scope.clearChart);
     };
 
     $scope.watchList.forEach(function(prop){
